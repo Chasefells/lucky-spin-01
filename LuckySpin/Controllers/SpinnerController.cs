8a3f85e93fe7a7c4790a56a37216a1106cdd8d7b
Chase <cfellstrom@gmail.com>
2025-01-21 11:21:46 -0800

    push

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -11,11 +11,26 @@ namespace LuckySpin.Controllers
 
         public IActionResult Index(int luck) 
         {
-            //TODO: add your string builder and HTML
+            //done: add your string builder and HTML
+            var rand = new Random();
+            int[] spin = new int[] { rand.Next(0,10), rand.Next(0,10), rand.Next(0,10) };
 
+System.Text.StringBuilder htmlToShow =  
+            new System.Text.StringBuilder("<body><h1>Lucky Spin</h1><button onclick='history.go(0)'>Spin</button>");
+          htmlToShow.Append("<div>" + spin[0] + "</div>");
+          htmlToShow.Append("<div>" + spin[1] + "</div>");
+          htmlToShow.Append("<div>" + spin[2] + "</div>");
+    if(spin[0] == luck  || spin[1] == luck  || spin[2] == luck) {
+
+          htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");
+          };
 
             //TODO: Modify this to use the string builder's response string as the Content property's value
-            return new ContentResult { Content = "<h1>We're Ready to Spin with Controllers</h1>", ContentType="text/html"};
+                return new ContentResult 
+            { 
+                Content = htmlToShow.ToString(), 
+                ContentType = "text/html" 
+            };
         }
     }
 }
--- LuckySpin/Program.cs
+++ LuckySpin/Program.cs
@@ -1,9 +1,9 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Install Services using the builder.Services methods
-  //  TODO: add the "Controllers" method to the "builder" to enable MVC controllers
+  //  Done: add the "Controllers" method to the "builder" to enable MVC controllers
  
-
+builder.Services.AddControllersWithViews();
 //Builds the app with the added services
 var app = builder.Build();
 
@@ -16,22 +16,21 @@ app.UseStaticFiles();
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Spinner/Error");
 }
-//   TODO: add Routing to recognize custom "Routes" in place of folders and files
-
+//   Done: add Routing to recognize custom "Routes" in place of folders and files
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action}/{luck}",
-    defaults: new
-    {
-        controller = "Spinner",
-        action = "Index",
-        luck = 7
-    });
+    name: "default", // Default route
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 //Configure Routing with a general pattern and a default setting if the URL path is left out
- // FIXME:if your have time, add a range(1,9) method to constrain luck between 1 and 9
+ // Done:if your have time, add a range(1,9) method to constrain luck between 1 and 9
 
-
+app.MapControllerRoute(
+    name: "luckRoute", 
+    pattern: "spin/{luck:int:min(1):max(9)}",
+    defaults: new { controller = "Spin", action = "Index" });
+   
 
 app.Run();
+
 
